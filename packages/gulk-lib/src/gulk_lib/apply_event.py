a27ca232eb4e500d087b28574cf61630e429bb4b
Ryan <rpbiwer@gmail.com>
2026-07-29 18:09:02 -0400

    Install Pyright, mainly for match exhaustiveness checking

--- packages/gulk-lib/src/gulk_lib/apply_event.py
+++ packages/gulk-lib/src/gulk_lib/apply_event.py
@@ -1,3 +1,5 @@
+from typing import assert_never
+
 from gulk_lib.events import GameEvent, NewGame
 from gulk_lib.game_state import GameState
 
@@ -7,3 +9,5 @@ def apply_event(state: GameState, event: GameEvent):
         case NewGame(config):
             assert state.config is None
             state.config = config
+        case _:
+            assert_never(event)
